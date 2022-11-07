e4ddd2ce50e4d7f63b1a403ad252a09145e53578
Kevin Laborda <klaborda@gmail.com>
2022-11-06 18:13:01 -0700

    chore: better keyboard training

--- config.symlink/nvim/after/plugin/keymap/init.lua
+++ config.symlink/nvim/after/plugin/keymap/init.lua
@@ -5,9 +5,28 @@ local inoremap = Remap.inoremap
 
 nnoremap("<leader>qw", ":Ex<CR>")
 
-inoremap("<leader>jk", "<esc>")
+inoremap("<leader>jj", "<esc>")
+
+-- keyboard training
+nnoremap("<up>", "<nop>")
+nnoremap("<down>", "<nop>")
+nnoremap("<left>", "<nop>")
+nnoremap("<right>", "<nop>")
+inoremap("<up>", "<nop>")
+inoremap("<down>", "<nop>")
+inoremap("<left>", "<nop>")
+inoremap("<right>", "<nop>")
+
+-- open new window and switch to it
+nnoremap("<leader>w", "<C-w>v<C-w>l")
+-- quick navigate around windows
+nnoremap("<C-h>", "<C-w>h")
+nnoremap("<C-j>", "<C-w>j")
+nnoremap("<C-k>", "<C-w>k")
+nnoremap("<C-l>", "<C-w>l")
 
 -- greatest remap ever
 xnoremap("<leader>p", "\"_dP")
 
+-- make executable
 nnoremap("<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
